09a90d220236fd04fefa1b2559eee7566b77abf3
MaldifassiJacopo <maldifassi.jacopo@iisgalvanimi.edu.it>
2023-03-23 14:04:20 +0000

    primo_commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Macchine } from './macchine/macchine.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '01_Angular_empty';
+  macchine:Macchine[]
+  constructor(){
+    this.macchine = [
+      
+        {
+            "id": 1,
+            "modello": "127",
+            "marca": "Fiat",
+            "prezzo": 1300,
+            "velocitamax": 110
+    
+        },
+    
+        {
+            "id": 2,
+            "modello": "Giulietta",
+            "marca": "Alfa Romeo",
+            "prezzo": 4000,
+            "velocitamax": 180
+        },
+        {
+            "id": 3,
+            "modello": "Fiesta",
+            "marca": "Ford",
+            "prezzo": 1600,
+            "velocitamax": 130
+    
+        },
+        {
+            "id": 4,
+            "modello": "Baracca",
+            "marca": "Subaru",
+            "prezzo": 5500,
+            "velocitamax": 170
+    
+        }
+    ]
+  
+  }
+  sortedMacchine(): Macchine[] {
+    return this.macchine.sort((a: Macchine, b: Macchine) => b.velocitamax);
+  }
+  stampArray() {
+    this.macchine.sort((a, b) => b.velocitamax - a.velocitamax);
+  }
+
+  stampArraylento(){
+    this.macchine.sort((a,b) => a.velocitamax - b.velocitamax )
+  }
 }
---
+++ src/app/macchine/macchine.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Macchine } from './macchine.model';
+@Component({
+  selector: 'app-macchine',
+  templateUrl: './macchine.component.html',
+  styleUrls: ['./macchine.component.css']
+})
+export class MacchineComponent {
+  @Input() macchina:Macchine;
+  
+  ngOnInit() {}
+
+}
+
